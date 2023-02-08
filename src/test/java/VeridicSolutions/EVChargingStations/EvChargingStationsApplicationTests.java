d6dd8179644dd0a0493dc61d679f72ad1404a6cd
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-02-08 19:58:11 +0530

    Welcome

---
+++ src/test/java/VeridicSolutions/EVChargingStations/EvChargingStationsApplicationTests.java
@@ -0,0 +1,13 @@
+package VeridicSolutions.EVChargingStations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargingStationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
