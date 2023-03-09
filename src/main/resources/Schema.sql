ce912006b47ee29c1be33797d2acac25a4dbdb28
Nikhil Rai <Nikhil.Rai@Polestarllp.local>
2023-03-09 11:48:24 +0530

    Created table Station

---
+++ src/main/resources/Schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE STATION
+(
+    station_id varchar(MAX),
+    station_name varchar(MAX),
+    station_pricing varchar(MAX),
+    station_image varchar(MAX),
+    station_address varchar(MAX)
+
+);
